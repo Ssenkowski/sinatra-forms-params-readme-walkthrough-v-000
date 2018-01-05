023bf14ab24a439a169cf395800a6dd6066a1dfe
Steven Senkowski <scsenkowski@gmail.com>
2018-01-05 03:26:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,9 @@ class App < Sinatra::Base
     erb :food_form
   end
 
-  # Add your post route and action below
-  <form method="POST" action="/food">
+  # Add your post route and action below #How do I know where to put it?
+  post '/food' do
+    <form method="POST" action="/food">
+  end
 
 end
